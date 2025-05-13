1e1177ef9efd1ccaa4cacb6d50b845fcde1fa19b
Fork (Liam) <liamjones989@outlook.com>
2025-05-13 08:25:06 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function launchTabs() {
+  for (let i = 0; i < 5; i++) {
+    let win = window.open('', '', `width=300,height=300,left=${Math.random()*800},top=${Math.random()*500}`);
+    if (win) {
+      win.document.write(`
+        <html>
+        <head><title>Tab ${i + 1}</title></head>
+        <body style="background-color: #${Math.floor(Math.random()*16777215).toString(16)};">
+          <h1>I'm tab ${i + 1}!</h1>
+          <script>
+            setInterval(() => {
+              window.moveBy(Math.random() * 20 - 10, Math.random() * 20 - 10);
+            }, 100);
+          <\/script>
+        </body>
+        </html>
+      `);
+    } else {
+      alert("Pop-up blocked! Please allow pop-ups for this site.");
+    }
+  }
+}
 
