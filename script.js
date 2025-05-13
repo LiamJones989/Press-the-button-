90125ffa3444f3c71fcd88618958631199b061f5
Fork (Liam) <liamjones989@outlook.com>
2025-05-13 08:30:18 -0500

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function launchTabs() {
-  for (let i = 0; i < 5; i++) {
-    let win = window.open('', '', `width=300,height=300,left=${Math.random()*800},top=${Math.random()*500}`);
+  for (let i = 0; i < 25; i++) {
+    let win = window.open('', '', `width=200,height=200,left=${Math.random()*1000},top=${Math.random()*600}`);
     if (win) {
       win.document.write(`
         <html>
         <head><title>Tab ${i + 1}</title></head>
-        <body style="background-color: #${Math.floor(Math.random()*16777215).toString(16)};">
-          <h1>I'm tab ${i + 1}!</h1>
+        <body style="background-color: #${Math.floor(Math.random()*16777215).toString(16)}; margin:0;">
+          <h1 style="text-align:center; font-size:20px;">Tab ${i + 1}</h1>
           <script>
             setInterval(() => {
-              window.moveBy(Math.random() * 20 - 10, Math.random() * 20 - 10);
-            }, 100);
+              window.moveBy(Math.random() * 100 - 50, Math.random() * 100 - 50);
+            }, 50);
           <\/script>
         </body>
         </html>
       `);
     } else {
-      alert("Pop-up blocked! Please allow pop-ups for this site.");
+      alert("Pop-up blocked! Please enable pop-ups for this site.");
+      break;
     }
   }
 }
-
